9a4cd6a4617f81b4fc6284dd8faebf2a2ca89304
MiscellaneousStuff <joe.makepeace01@gmail.com>
2023-03-30 20:25:33 +0100

    Turns out its pretty hard lol

---
+++ dataset/main.py
@@ -0,0 +1,17 @@
+# Print hi
+print("hi")
+
+# Print hello
+print("hello")
+
+# Print yah
+print("yah")
+
+# Print bloody hell
+print("bloody hell")
+
+# Print python
+print("python")
+
+# Print whaa
+print("whaa")
